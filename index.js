dd1af16da595e9dc54182b2fa794dca661d9c02b
danielmcv <daneil625@outlook.com>
2017-05-20 23:29:08 -0600

    Edite el archivo JS

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 // Primero hay que entender que es una Variable.
 // ¿Que es una variable?
 // Una variable se usa para guardar informacion para poder utilizarla despues en un programa.
+//Las variables son locaciones reservadas en la memoria 
+//de una computadora para almacenar un valor.
+//Puedes almacenar diferentes tipos de datos, tales como,
+//numeros, enteros, coma flotantes, una cadena de caracteres, etc.
 
-// Conocer los tipos de datos es muy importante.
+// ¿Que es un identificador?
+// Es el nombre que se le da a una variable o a otras entidades tales
+// como, funciones, objetos, etc.
+
+// Conocer los tipos de datos es muy importante.++
 //Data Types / Tipo de datos
 
 // --- NUMBER 
@@ -14,7 +22,7 @@
 //STRING 'hola mi nombre es Daniel' <--- se caracterizan por que estan dentro de comillas ''.
 //Se pueden hacre estas operaciones basicas y mas --->  +, -. * , / 
 
-var numero1 = 22;
+var numero1 = 46;
 var numero2 = 18;
 	alert(numero1 + numero2 + 4 + 5);
 	alert('Hola yo tengo ' + numero1 + ' años');
@@ -27,9 +35,9 @@ var numero2 = 18;
 	// Son "CASE SENSITIVE" <--- significa que va a respetar las mayusculas y minusculas.
 
 
-var prueba = 'Estamos probando';
+var Prueba = 'Estamos probando';
 var Prueba = 'Esta es otra prueba';
-	alert(prueba);
+	alert(Prueba);
 
 
 //Estilos de Variables
@@ -49,10 +57,10 @@ var Prueba = 'Esta es otra prueba';
 	var colores = ['azul', 'verde', 'rojo', 'amrillo'];
 		colores.push('blanco');
 
-		alert(colores);
+		alert(colores.length);
 
 
-	var numeros = [3,5,6,7,4,3,9,8,6,4,4,'siete']; //<--- tambien puedes agregar STRINGS 'SEVEN'
+	var numeros = [3,5,6,7,4,3,9,8,6,4,4,'siete','nueve']; //<--- tambien puedes agregar STRINGS 'SEVEN'
 		alert(numeros[3] + numeros[8]);
 		alert(numeros.sort());
 		alert(numeros.reverse());
@@ -62,14 +70,14 @@ var Prueba = 'Esta es otra prueba';
 //LOOPS = for, while, for each.
 
  console.log('HOLA');     // <--- se usa (console.log) para arrojar el resultado e inspeccionar el elemento de tu nabegador.
-	for (var edad = 1; edad <= 20; edad++){
+	for (var edad = 1; edad < 20; edad++){
 		console.log('Daniel tiene '+ edad + ' años');
 	}
 
 
 	var edad = 1;
 	while(edad <= 10){
-		console.log(edad);
+		console.log('Daniel tiene '+ edad + ' años');
 		edad++;
 	}
 
@@ -80,17 +88,21 @@ var Prueba = 'Esta es otra prueba';
 	numeros.forEach(function(numero){
 		console.log(numero);
 	});
+	// Esto es Lo mismo 
 
+	for (var i = 0; i <= numeros.length -1; i++){
+		 console.log(numeros[i]);
+		};
 //=============================================================================================================
 // CONDITIONALS / CONDICIONALES 
 
-if (1 == 3) {                         //<--- tambien puedes hacer esto != y te dara falso.
+if (3 == 3) {                         //<--- tambien puedes hacer esto != y te dara falso.
 	console.log('esto es verdadero');
 };
 
 var num1 = 5;
 var num2 = 5;
-if(num1 == num2 &&  num1 == 5){       //<--- se usa (&& como and) y (|| como or).
+if(num1 == num2 ||  num1 == 4){       //<--- se usa (&& como and) y (|| como or).
 	console.log('Esto es verdadero');
 } else{
 	console.log('Esto es falso');
@@ -119,19 +131,20 @@ var persona = {
 		return this.nombre + ' ' + this.apellido;   //<--- La palabra (this) se usa para referenciar al objeto persona en este caso.
 	}
 }
-console.log(persona.apellido)
+console.log(persona.nombreCompleto());
 
 //Object constructor
 
 var television = new Object();
 television.color = 'negra';
 television.shape = 'cuadrada';
+television.tamaño = '30'
 
 television.descripcion = function(){
 	return 'Esta television es de color '+ this.color + ' y es '
-	+ this.shape;
+	+ this.shape +' '+ this.tamaño;
 }
-console.log(television.descripcion())
+document.write(television.descripcion())
 
 
 //=============================================================================================================
@@ -142,3 +155,7 @@ function cambiarTexto(){
 	var heading = document.getElementById('heading');
 	heading.innerHTML = 'HAZ CAMBIADO EL TITULO MUCHAS FELICIDADES! :)';
 }
+
+var celcius = 34
+var fahrenheit = ((celcius*9)/5) +32
+console.log (fahrenheit)
